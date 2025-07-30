7069a7a27bb05466b77d7554b602b31906ca4623
Satinder Singh <ssingh20@promethium.cirg.washington.edu>
2025-07-30 10:25:45 -0700

    Local changes to Docker and frontend config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || '/',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
+  },
 })
